146046365abcf66e5843c6999dcd3f25c29e21e3
tybanash <awahnadege02@gmail.com>
2022-08-30 15:15:08 -0400

    "Completed day 1 exercises"

---
+++ day_1/helloworld.py
@@ -0,0 +1,57 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+# Use operands 3 and 4 to do the following operations
+import math #imports math module
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print() # prints an empty line
+
+# Write strings
+print('Nadege')
+print('A')
+print('Canada')
+print('I am enjoying 30 days of python')
+print() # prints an empty line
+
+# Check data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Nadege'))
+print(type('A'))
+print(type('Cameroon'))
+print() # prints an empty line
+
+# Examples of Boolean, List, Ductionary, Set and Tuple
+print(type(True))
+print(type([2, 4, 6, 8, 10]))
+print(type({
+    'country':'Canada',
+    'province':'Ontario',
+    'region':'Durham',
+    'city':'Pickering'
+    }))
+print(type({1,3,5,7,9}))
+print(type(('North York', 'Scarborough', 'Mississauga', 'Brampton')))
+print() # prints an empty line
+ 
+
+ # √[(x2 – x1)2 + (y2 – y1)2].
+# Find a Euclidian distance between (2, 3) and (10, 8)
+x1 = 2
+y1 = 3
+x2 = 10
+y2 = 8
+b = (10-2) ** 2
+c = (8-3) ** 2
+print(math.sqrt(b+c))
+
+
--- helloworld.py
+++ helloworld.py
@@ -17,3 +17,5 @@ print(type([1, 2, 3]))   # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+# Exercise: Level 1
